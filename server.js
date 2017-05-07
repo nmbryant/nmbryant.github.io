a20add2b9b7c48b39a3ee0b1178672acea7b5f49
nmbryant <nmbryant@wpi.edu>
2017-05-06 22:11:29 -0400

    Setup for heroku

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var path = require('path');
+var app = express()
+
+app.use(express.static(path.join(__dirname, '/')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.listen(8080, function () {
+    console.log('Example app listening on port 3000!')
+})
